aad1f9acc44970914fa2a61afaf212c591c1b8ff
Adityesh <adityeshcool@gmail.com>
2019-10-24 23:20:16 +0530

    Added buttons for app

--- app.py
+++ app.py
@@ -10,4 +10,11 @@ canvas.pack()
 frame = tk.Frame(root,bg="white")
 frame.place(relwidth=0.8,relheight=0.8,relx=0.1,rely=0.1)
 
+button = tk.Button(root,text="Open App",padx=10,pady=5, fg="white", bg="#243d45")
+button.pack()
+
+
+runApp = tk.Button(root,text="Run App",padx=10,pady=5, fg="white", bg="#243d45")
+runApp.pack()
+
 root.mainloop()
