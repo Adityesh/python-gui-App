d6ce3d17659679c15213f6fc18fb4a56a20f6630
Adityesh <adityeshcool@gmail.com>
2019-10-24 23:30:59 +0530

    run and open app button

--- app.py
+++ app.py
@@ -3,6 +3,23 @@ from tkinter import filedialog, Text
 import os
 
 root = tk.Tk()
+apps = []
+
+def addApp():
+for widget in frame.winfo_chidlren():
+    widget.destroy()
+
+    filename = filedialog.askopenfilename(initialdir="/",title="Select File",filetypes=(("executables","*.exe"),("all files","*.*")))
+
+    apps.append(filename)
+
+    for app in apps:
+        label = tk.Label(frame,text=app,bg+"gray")
+        label.pack()
+
+def runApps():
+    for app in apps:
+        os.startfile(app)
 
 canvas = tk.Canvas(root,height = 700,width = 400,bg = "#243d45")
 canvas.pack()
@@ -10,11 +27,11 @@ canvas.pack()
 frame = tk.Frame(root,bg="white")
 frame.place(relwidth=0.8,relheight=0.8,relx=0.1,rely=0.1)
 
-button = tk.Button(root,text="Open App",padx=10,pady=5, fg="white", bg="#243d45")
+button = tk.Button(root,text="Open App",padx=10,pady=5, fg="white", bg="#243d45",command = addApp)
 button.pack()
 
 
-runApp = tk.Button(root,text="Run App",padx=10,pady=5, fg="white", bg="#243d45")
+runApp = tk.Button(root,text="Run App",padx=12,pady=5, fg="white", bg="#243d45",command = runApps)
 runApp.pack()
 
 root.mainloop()
